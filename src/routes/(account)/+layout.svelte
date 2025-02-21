24caf0bba31de89949acd868e50a8b232844e0d6
Trevor <65660736+realtrev@users.noreply.github.com>
2025-02-20 21:09:48 -0500

    Add listing management for organizations

--- src/routes/(account)/+layout.svelte
+++ src/routes/(account)/+layout.svelte
@@ -8,7 +8,7 @@
   } = $props();
 </script>
 
-<div class="w-full h-min min-h-0 sm:min-h-screen sm:flex bg-white justify-center items-center sm:bg-gradient-to-tl sm:from-rose-500 sm:to-primary-5">
+<div class="w-full h-min min-h-0 sm:min-h-screen sm:flex bg-white justify-center items-center sm:bg-gradient-to-tl sm:from-blue-900 sm:to-teal-500">
   <div id="form" class="bg-white w-screen sm:max-w-[32rem] shrink-0 h-min sm:rounded-2xl flex items-center sm:my-8 sm:shadow-2xl">
     {@render children()}
   </div>
